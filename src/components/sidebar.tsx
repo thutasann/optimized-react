41dd8556f9d83af17d2643fd8cd19c737ec49872
thutasann <thutasann2002@gmail.com>
2023-03-10 22:52:57 +0630

    sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useLayoutEffect, useState } from 'react'
 import { Link, useLocation, useParams } from 'react-router-dom'
 import { SideBarData } from '../utils/sidebar'
 
@@ -9,17 +9,29 @@ interface Props {
 
 const SideBar: React.FC<Props> = ({ children }) => {
   const currentRoute = useLocation()
+  const window = document.getElementById('root') as HTMLElement
 
+  useLayoutEffect(() => {
+    if (window.clientWidth > 375) {
+      setOpen(true)
+    } else {
+      setOpen(false)
+    }
+  }, [window.clientWidth])
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  console.log('open', open)
   return (
     <>
       <button
+        onClick={() => setOpen(prev => !prev)}
         data-drawer-target='default-sidebar'
         data-drawer-toggle='default-sidebar'
         aria-controls='default-sidebar'
         type='button'
-        className='inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+        className='inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
       >
-        <span className='sr-only'>Open sidebar</span>
         <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
           <path
             clipRule='evenodd'
@@ -29,48 +41,57 @@ const SideBar: React.FC<Props> = ({ children }) => {
         </svg>
       </button>
 
-      <aside
-        id='default-sidebar'
-        className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0'
-        aria-label='Sidebar'
-      >
-        <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'>
-          <ul className='space-y-2'>
-            {SideBarData.map(sidebar => (
-              <li>
-                <Link
-                  to={sidebar.link}
-                  className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-700 ease-in-out ${
-                    sidebar.link === currentRoute.pathname && 'bg-gray-700'
-                  }`}
-                >
-                  {sidebar.isIndex ? (
-                    <svg
-                      aria-hidden='true'
-                      className='w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
-                      fill='currentColor'
-                      viewBox='0 0 20 20'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z'></path>
-                      <path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z'></path>
-                    </svg>
-                  ) : (
-                    <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
-                      <path
-                        clipRule='evenodd'
-                        fillRule='evenodd'
-                        d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'
-                      ></path>
-                    </svg>
-                  )}
-                  <span className={`ml-3 font-bold ${sidebar.isIndex && 'uppercase'}`}>{sidebar.name}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </aside>
+      {open && (
+        <aside
+          id='default-sidebar'
+          className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform  sm:translate-x-0`}
+          aria-label='Sidebar'
+        >
+          <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'>
+            <ul className='space-y-2'>
+              {SideBarData.map(sidebar => (
+                <li key={sidebar.link}>
+                  <Link
+                    to={sidebar.link}
+                    className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-700 ease-in-out ${
+                      sidebar.link === currentRoute.pathname && 'bg-gray-700'
+                    }`}
+                    onClick={() => setOpen(false)}
+                  >
+                    {sidebar.isIndex ? (
+                      <svg
+                        aria-hidden='true'
+                        className='w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
+                        fill='currentColor'
+                        viewBox='0 0 20 20'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path d='M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z'></path>
+                        <path d='M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z'></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        className='w-6 h-6'
+                        aria-hidden='true'
+                        fill='currentColor'
+                        viewBox='0 0 20 20'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          clipRule='evenodd'
+                          fillRule='evenodd'
+                          d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'
+                        ></path>
+                      </svg>
+                    )}
+                    <span className={`ml-3 font-bold ${sidebar.isIndex && 'uppercase'}`}>{sidebar.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </aside>
+      )}
 
       <div className='p-4 sm:ml-64'>{children}</div>
     </>

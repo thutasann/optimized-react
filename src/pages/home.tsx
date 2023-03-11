ba796266f1e84af307c0bf85093b4eecc6a508ef
thutasann <thutasann2002@gmail.com>
2023-03-11 14:32:21 +0630

    feat: useMemo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <h1 className='text-4xl font-bold'>OPTIMIZED REACT</h1>
       <p className='text-xl text-center'>This site contains Optimization Techniques for React, React Hook and React Redux.</p>
       <Link
-        to={SideBarData[1].link}
+        to={SideBarData[0].link}
         className='bg-gray-700 px-3 py-2 mt-5 rounded-md font-[600] text-sm hover:bg-gray-600 transition-all duration-700 ease-in-out flex items-center gap-2 group'
       >
         Let Get Started <BsArrowRightCircleFill size={19} className='group-hover:ml-2 transition-all duration-300 ease-in-out' />
--- src/pages/use-memo.tsx
+++ src/pages/use-memo.tsx
@@ -1,11 +1,50 @@
+import { count } from 'console'
+import { memo, useMemo, useState } from 'react'
+import Button from '../common/button'
+import FlexBox from '../common/flex-box'
 import { Heading } from '../common/heading'
+import Input from '../common/input'
+
+function factorial(n: number): number {
+  let i = 0
+  while (i < 20000000) i++
+  if (n < 0) {
+    return -1
+  }
+  if (n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
+}
 
 const UseMemo = () => {
+  const [counter, setCounter] = useState(1)
+  const [name, setName] = useState('')
+
+  const result = useMemo(() => {
+    return factorial(counter)
+  }, [counter])
+
   return (
     <div>
-      <Heading text='Use Memo' description='In this section, Use Memo Hook will be applied to optimize the Re-rendering.' />
+      <Heading text='React Memo' description='In this section, React Memo will be applied to optimize the Heavy Functions' />
+      <FlexBox>
+        <Button text='Decrement' onClick={() => setCounter(counter - 1)} />
+        <Button text='Increment' onClick={() => setCounter(counter + 1)} />
+      </FlexBox>
+      <p>
+        Fractroial of {counter} is {result}
+      </p>
+      <div className='mt-4'>
+        <Input type='text' placholder='Enter Name' value={name} onChange={(e: any) => setName(e.target.value)} />
+        <DisplayName name={name} />
+      </div>
     </div>
   )
 }
 
+const DisplayName = memo(({ name }: any) => {
+  console.log('Rendered')
+  return <p>{`My name is ${name}`}</p>
+})
 export default UseMemo
